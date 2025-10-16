8875711e544aee143e483ca912a8e31f935ff6df
qinyuelong <qyl@ywhc.com>
2025-10-16 17:35:42 +0800

    只在编辑表格数据时，增加防止子组件修改影响原始数据

--- src/pages/test/enterprise/EnterprisePage.vue
+++ src/pages/test/enterprise/EnterprisePage.vue
@@ -247,21 +247,20 @@ const resetQuery = () => {
   loadEnterprises()
 }
 
+const showEnterpriseDetail = (enterprise) => {
+  currentEnterprise.value = enterprise
+  dialogMode.value = 'view'
+  enterpriseDialog.value = true
+}
+
 /**
  *  引用传递 vs 值传递
  * 深拷贝方式，创建新对象 JSON.parse(JSON.stringify(enterprise)) 
  *  在父组件传递数据时进行深拷贝，这样子组件的修改就不会影响到原始数据
  */
-const showEnterpriseDetail = (enterprise) => {
-
+const showEnterpriseEdit = (enterprise) => {
   // JSON.parse(JSON.stringify(enterprise)) 
   // currentEnterprise.value = enterprise // 引用传递
-  currentEnterprise.value = JSON.parse(JSON.stringify(enterprise)) // currentEnterprise全新的独立对象
-  dialogMode.value = 'view'
-  enterpriseDialog.value = true
-}
-
-const showEnterpriseEdit = (enterprise) => {
   currentEnterprise.value = JSON.parse(JSON.stringify(enterprise))
   dialogMode.value = 'edit'
   enterpriseDialog.value = true
